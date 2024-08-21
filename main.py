f939a387eba18fd7152953df36bcd49e29b7d001
Phil Tran <7899624+LeoBartenev@users.noreply.github.com>
2024-08-21 21:15:52 +0100

    added button func for monster maff

--- main.py
+++ main.py
@@ -39,6 +39,8 @@ class Worker:
         self.leftToPay = 0
         self.__class__.instances.append(self)
 
+    # TODO create a reset instances method
+
 
 
 
@@ -102,6 +104,7 @@ with st.form("my_form2"):
     twenty_input = st.text_input(label='20:', value=0)
     ten_input = st.text_input(label="10:", value=0)
     five_input = st.text_input(label='5:', value=0)
+    two_input = st.text_input(label='2:', value=0)
     one_input = st.text_input(label='1:', value=0)
     fiftyP_input = st.text_input(label='0.50:', value=0)
     twentyP_input = st.text_input(label='0.20:', value=0)
@@ -118,17 +121,21 @@ with st.form("my_form2"):
         if twenty_input == "": twenty_input = 0
         if ten_input == "": ten_input = 0
         if five_input == "": five_input = 0
+        if two_input == "": one_input = 0
         if one_input == "": one_input = 0
         if fiftyP_input == "": fiftyP_input = 0
         if twentyP_input == "": twentyP_input = 0
 
-        st.session_state.money[0] = {"50": int(fifty_input), "20": int(twenty_input),
-             "10": int(ten_input), "5": int(five_input),
-             "1": int(one_input), "0.50": int(fiftyP_input), "0.20": int(twentyP_input)}
+        st.session_state.money[0] = {
+            "50": int(fifty_input), "20": int(twenty_input),
+            "10": int(ten_input), "5": int(five_input),
+            "1": int(one_input), "2": int(two_input),
+            "0.50": int(fiftyP_input), "0.20": int(twentyP_input)}
         total = (st.session_state.money[0].get("50") * 50 +
                  st.session_state.money[0].get("20") * 20 +
                  st.session_state.money[0].get("10") * 10 +
                  st.session_state.money[0].get("5") * 5 +
+                 st.session_state.money[0].get("2") * 2 +
                  st.session_state.money[0].get("1") * 1 +
                  st.session_state.money[0].get("0.50") * 0.5 +
                  st.session_state.money[0].get("0.20") * 0.2)
@@ -137,10 +144,124 @@ dfMoney = pd.DataFrame(
    st.session_state.money
 )
 st.dataframe(dfMoney, use_container_width=True)
-st.write(st.session_state.money)
 
 
 st.write(f"total is {total}")
 
 
+
+st.write(st.session_state.money)
 st.write(st.session_state.instances)
+
+def sortBiggerFirst(listOfWorkers):
+    return sorted(listOfWorkers, key=lambda worker: worker.leftToPay, reverse=True)
+
+def splitTips():
+    # create the tip pool
+    twentyP = Tender(st.session_state.money[0].get("0.20"), 0.2, 7)
+    fiftyP = Tender(st.session_state.money[0].get("0.50"), 0.5, 6)
+    pound = Tender(st.session_state.money[0].get("1"), 1, 5)
+    twoPound = Tender(st.session_state.money[0].get("2"), 2, 4)
+    fivePound = Tender(st.session_state.money[0].get("5"), 5, 3)
+    tenPound = Tender(st.session_state.money[0].get("10"), 10, 2)
+    twentyPound = Tender(st.session_state.money[0].get("20"), 20, 1)
+    fiftyPound = Tender(st.session_state.money[0].get("50"), 50, 0)
+
+    cashList = [fiftyPound, twentyPound, tenPound, fivePound, twoPound, pound, fiftyP, twentyP]
+
+     # believe it or not, this loops through the statesessions worker values
+    # to create workers objects
+    instances = {}
+    listOfValues = []
+
+    for entry in st.session_state.instances:
+        name = entry.get("name")
+        hours = float(entry.get("hours"))
+        instances[name] = Worker(name, hours, 1)
+
+
+
+    listOfWorkers = Worker.instances
+
+    totalTips = 0
+    for tender in cashList:
+        totalTips += tender.value * tender.quantity
+    totalLeftToPay = totalTips
+    st.write(f'totaltips is {totalTips}')
+
+    totalHours = 0
+    for worker in listOfWorkers:
+        totalHours += int(worker.hours) * worker.rate
+    st.write(f'total full rate hour is {totalHours}')
+
+    for worker in listOfWorkers:
+        worker.ratioOfTotalTip = worker.hours * worker.rate / totalHours
+        worker.totalTip = totalTips * worker.ratioOfTotalTip
+        worker.leftToPay = worker.totalTip
+        st.write(
+            f'{worker.name} percent of total tip is {worker.ratioOfTotalTip * 100}%, its totalTip is {worker.totalTip}')
+
+    st.write(f'total hours is {totalHours}, total tips is {totalTips}')
+
+    test = 0
+    for worker in listOfWorkers:
+        test += worker.totalTip
+
+    st.write(f'total of all worker tips is {test}')
+
+    sortedList = sorted(listOfWorkers, key=lambda worker: worker.totalTip, reverse=True)
+    totalWorkers = len(sortedList)
+
+    for tender in cashList:
+        st.write(f'there is {tender.quantity} units of {tender.value} value')
+        while (tender.quantity > 0):
+            tempListOfWorkers = sortBiggerFirst(listOfWorkers)
+            for worker in tempListOfWorkers:
+                if worker.leftToPay >= tender.value:
+                    worker.breakdown[tender.indexForBreakdown] += 1
+                    worker.leftToPay -= tender.value
+                    tender.quantity -= 1
+                    # print(
+                    #    f'left to pay for {worker.name} is {worker.leftToPay} breakdown is {worker.breakdown} current tender is {tender.value}')
+                    if tender.quantity == 0:
+                        break
+                else:
+                    break
+
+            if (tender.value < 1) & (tempListOfWorkers[0].leftToPay < 1):
+                break
+            if (tender.value > tempListOfWorkers[0].leftToPay):
+                break
+
+    for worker in listOfWorkers:
+        st.write(f'\nWorker {worker.name} is owed {worker.totalTip} by the pool')
+        totalWorkerTip = 0
+        index = 0
+        for tender in worker.breakdown:
+            totalWorkerTip += tender * cashList[index].value
+            index += 1
+        st.write(f'its been awarded {totalWorkerTip}, with {worker.leftToPay} left')
+        worker.tipsPaid = totalWorkerTip
+        for tender in cashList:
+            st.write(
+                f'Worker {worker.name} was awarded {worker.breakdown[cashList.index(tender)]} tenders of {tender.value} value')
+    st.write([tender.quantity for tender in cashList])
+
+    for instances in Worker.instances:
+        whiteSpace = 20 - len(instances.name)
+        st.write(
+            f'{whiteSpace * " "}{instances.name} is owed {round(instances.totalTip, 2)} pounds, and has been paid {instances.tipsPaid} bill breakdown is {instances.breakdown[0:4]}, coins is {instances.breakdown[4:]}')
+
+    doubleCheckTotal = 0
+    paidTips = 0
+
+    for worker in Worker.instances:
+        doubleCheckTotal += worker.totalTip
+        paidTips += worker.totalTip - worker.leftToPay
+
+    st.write(f'\nsum of all owed tips is {totalTips}')
+    st.write(f'sum of all total tips by instance is {doubleCheckTotal}')
+    st.write(f'sum of all paid tips is {paidTips}')
+
+if st.button("DO IT"):
+    splitTips()
