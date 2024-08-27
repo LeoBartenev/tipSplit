0b3d2b54a5d41064ed04a5df7d3ab7b03cc600ee
Phil Tran <7899624+LeoBartenev@users.noreply.github.com>
2024-08-27 12:24:26 +0100

    simplified UI / removed clutter

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 if "instances" not in st.session_state:
-    st.session_state.instances = []
+    st.session_state.instances = [{"name": "name", "hours": "0"}]
 
 if "previousInstances" not in st.session_state:
     st.session_state.previousInstances = []
@@ -23,6 +23,7 @@ if "workerFilledFlag" not in st.session_state:
 if "moneyFilledFlag" not in st.session_state:
     st.session_state.moneyFilledFlag = False
 
+
 class Worker:
     # this is the class that holds the data concerning each worker
     instances = []
@@ -41,10 +42,6 @@ class Worker:
         self.leftToPay = 0
         self.__class__.instances.append(self)
 
-    # TODO create a reset instances method
-
-
-
 
 class Tender:
     def __init__(self, quantity, value, indexForBreakdown):
@@ -52,47 +49,18 @@ class Tender:
         self.value = value
         self.indexForBreakdown = indexForBreakdown
 
-def add_worker(name, hours):
-    # adds a worker from session memory
-    if (isinstance(name, str)) and (isinstance(float(hours), float)):
-        st.write(name, "added.")
-        st.session_state.previousInstances.append(name)
-        st.session_state.instances.append({"name": name, "hours": hours})
-        st.session_state.workerFilledFlag = True
-        st.write(len(st.session_state.instances))
-        st.write(st.session_state.instances)
-    else:
-        st.write("Looks like the form isn't properly filled.")
 
-def remove_worker(name):
-    # removes the worker from session memory
-    if name in st.session_state.previousInstances:
-        st.write(name, "removed")
-        st.session_state.instances = [instance for instance in st.session_state.instances
-                                      if not (instance.get("name") == name)]
-        st.session_state.previousInstances.remove(name)
-        if len(st.session_state.instances) == 0:
-            st.session_state.workerFilledFlag = False
-        # rerun to display the data properly
-        st.rerun()
-    else:
-        st.write(name, " is not in the list")
+def update_worker_instances(data):
+    st.session_state.instances = data
+    st.session_state.workerFilledFlag = True
 
-def remove_last_worker_added():
-    # removes the last worker added with the top form
-    st.session_state.instances = [instance for instance in st.session_state.instances
-                                  if not (instance.get("name") == st.session_state.previousInstances[-1])]
-    st.session_state.previousInstances = st.session_state.previousInstances[:-1]
-    if len(st.session_state.instances) == 0:
-        st.session_state.workerFilledFlag = False
-    st.rerun()
 
 def reset_worker_instance_data():
-    #empty the currently held worker's datas
-    st.session_state.instances = []
-    st.session_state.previousInstances = []
+    # empty the currently held worker's datas
+    st.session_state.instances =  [{ "name" : "name", "hours" : "0"}]
     st.session_state.workerFilledFlag = False
     st.rerun()
+
 
 def update_money_instance(fifty, twenty, ten, five, two, one, fiftyP, twentyP):
     st.session_state.money[0] = {
@@ -103,8 +71,9 @@ def update_money_instance(fifty, twenty, ten, five, two, one, fiftyP, twentyP):
     st.write(f"total is {get_total_money()}")
     st.session_state.moneyFilledFlag = True
 
+
 def get_total_money():
-    #returns the sum of all tenders in st.session_state.money
+    # returns the sum of all tenders in st.session_state.money
     return (st.session_state.money[0].get("50") * 50 +
                  st.session_state.money[0].get("20") * 20 +
                  st.session_state.money[0].get("10") * 10 +
@@ -120,13 +89,13 @@ st.write("Because doing math after a long shift is not fun.")
 
 with st.expander("How to use this app"):
     st.write('''
-        First form has to be filled with a unique worker's name, and the number of hour worked. When you press 
-        "Add to list" you will see what you added in the table below.
+        You can use the file uploader to import a previous dataset. If you don't have one, you can create 
+        one in the next form"
     ''')
     st.write('''
-            The Second form lets you remove a worker from the table. Just enter the worker's name, and press 
-            "remove". Below the table, you also have a button that removes the last entered entry, and 
-            one that resets the whole thing.
+            The Second form create, add, edit data regarding the team you're splitting tips with.
+            Give a unique name for every worker, and write how many hours they worked on the right of the same line.
+            When everything is added, press update, and double check the infos in the summary below.
         ''')
     st.write('''
         The last form gathers the number of coins and bills that have to be added to the tip pool. 
@@ -147,62 +116,48 @@ with st.expander("How to use this app"):
        get the .csv extension, which makes it unrecognisable on your phone. To read it you need to add ".csv"
        to the end of the file name''')
 
-# this creates a form
-with st.form("my_form", clear_on_submit=True):
-    st.write("Fill this form for each worker")
-
-    name_input = st.text_input(label="Worker's name:")
-    hours_input = st.text_input(label='Hours worked:')
-
-    # this creates a button in the form
-    submitted = st.form_submit_button("Add to list")
-    if submitted:
-        add_worker(name_input, hours_input)
-
-
-with st.form("remover tool", clear_on_submit=True):
-    st.write("Fill this form to remove a worker from the list")
-
-    name_input = st.text_input(label="Worker's name:")
-
-    submitted = st.form_submit_button("remove")
-    if submitted:
-        remove_worker(name_input)
-
-
 uploaded_file = st.file_uploader("Upload an existing team log")
 if uploaded_file is not None:
     df1 = pd.read_csv(uploaded_file)
-    df1 = df1.iloc[:, 1:]
-    #if you upload a file you reset the session_state
+    # if you upload a file you reset the session_state
     st.session_state.instances = []
     for i, j in df1.iterrows():
-
-        #instances.append(Worker(j["name"], j["hours"], 1))
         nextIndex = str(len(st.session_state.instances))
         st.session_state.instances.append({
-            "name" : j["name"],
-            "hours" : j["hours"],
+            "name": j["name"],
+            "hours": j["hours"],
         })
-        #st.write(j["hours"])
-        #st.write(j["name"])
-    #st.write(df1)
-    #st.write(st.session_state.instances)
-
-
     st.session_state.workerFilledFlag = True
 
+
+st.write("You can add and edit worker's data here.")
+edited_df = st.data_editor(
+    st.session_state.instances,
+    key="editor",
+    num_rows="dynamic",
+    use_container_width=True,
+    column_config={
+        "name" : st.column_config.TextColumn("name"),
+        "hours" : st.column_config.NumberColumn("hours")
+    },
+
+
+)
+
+if st.button("Update"):
+    if edited_df is not None and not edited_df == st.session_state.instances:
+        # This will only run if
+        # 1. Some widget has been changed (including the dataframe editor), triggering a
+        # script rerun, and
+        # 2. The new dataframe value is different from the old value
+        update_worker_instances(edited_df)
+        st.rerun()
+
+
 st.write("You can see the workers you added here.")
-df = pd.DataFrame(st.session_state.instances)
-st.dataframe(df, use_container_width=True)
+df = pd.DataFrame(st.session_state.instances).iloc[:, 1:]
+st.dataframe(st.session_state.instances, use_container_width=True)
 
-# TODO use a data editor instead of a dataframe
-#st.data_editor(df, use_container_width=True)
-if st.button("remove last"):
-    remove_last_worker_added()
-
-if st.button("reset form"):
-    reset_worker_instance_data()
 
 with st.form("my_form2"):
     st.write("Fill this form with the collected tips")
@@ -218,7 +173,7 @@ with st.form("my_form2"):
     # Every form must have a submit button.
     submitted = st.form_submit_button("Update")
     if submitted:
-        #if empty string, return 0
+        # if empty string, return 0
         if fifty_input == "": fifty_input = 0
         if twenty_input == "": twenty_input = 0
         if ten_input == "": ten_input = 0
@@ -240,30 +195,27 @@ dfMoney = pd.DataFrame(
 st.dataframe(dfMoney, use_container_width=True)
 
 
-
-
-
-
 def sortBiggerFirst(listOfWorkers):
     # returns a list of Workers, biggest leftToPay first
     return sorted(listOfWorkers, key=lambda worker: worker.leftToPay, reverse=True)
 
+
 def splitTips():
-    #check if the forms have been filled
+    # check if the forms have been filled
     if st.session_state.moneyFilledFlag and st.session_state.workerFilledFlag:
         # create the tip pool
-        twentyP = Tender(st.session_state.money[0].get("0.20"), 0.2, 7)
-        fiftyP = Tender(st.session_state.money[0].get("0.50"), 0.5, 6)
+        twenty_p = Tender(st.session_state.money[0].get("0.20"), 0.2, 7)
+        fifty_p = Tender(st.session_state.money[0].get("0.50"), 0.5, 6)
         pound = Tender(st.session_state.money[0].get("1"), 1, 5)
-        twoPound = Tender(st.session_state.money[0].get("2"), 2, 4)
-        fivePound = Tender(st.session_state.money[0].get("5"), 5, 3)
-        tenPound = Tender(st.session_state.money[0].get("10"), 10, 2)
-        twentyPound = Tender(st.session_state.money[0].get("20"), 20, 1)
-        fiftyPound = Tender(st.session_state.money[0].get("50"), 50, 0)
+        two_pound = Tender(st.session_state.money[0].get("2"), 2, 4)
+        five_pound = Tender(st.session_state.money[0].get("5"), 5, 3)
+        ten_pound = Tender(st.session_state.money[0].get("10"), 10, 2)
+        twenty_pound = Tender(st.session_state.money[0].get("20"), 20, 1)
+        fifty_pound = Tender(st.session_state.money[0].get("50"), 50, 0)
 
-        cashList = [fiftyPound, twentyPound, tenPound, fivePound, twoPound, pound, fiftyP, twentyP]
+        cash_list = [fifty_pound, twenty_pound, ten_pound, five_pound, two_pound, pound, fifty_p, twenty_p]
 
-         # believe it or not, this loops through the statesessions worker values
+        # believe it or not, this loops through the statesessions worker values
         # to create workers objects
         instances = {}
         # creates the Worker objects
@@ -273,30 +225,30 @@ def splitTips():
             instances[name] = Worker(name, hours, 1)
 
         # Accessing the static list of workers
-        listOfWorkers = Worker.instances
+        list_of_workers = Worker.instances
 
-        totalTips = 0
-        for tender in cashList:
-            totalTips += tender.value * tender.quantity
-        st.write(f'total tips is {totalTips}')
+        total_tips = 0
+        for tender in cash_list:
+            total_tips += tender.value * tender.quantity
+        st.write(f'total tips is {total_tips}')
 
-        totalHours = 0
-        for worker in listOfWorkers:
-            totalHours += float(worker.hours) * worker.rate
-        st.write(f'total full rate hours is {totalHours}')
+        total_hours = 0
+        for worker in list_of_workers:
+            total_hours += float(worker.hours) * worker.rate
+        st.write(f'total full rate hours is {total_hours}')
 
         # initialise each worker split of the tip pool
-        for worker in listOfWorkers:
-            worker.ratioOfTotalTip = worker.hours * worker.rate / totalHours
-            worker.totalTip = totalTips * worker.ratioOfTotalTip
+        for worker in list_of_workers:
+            worker.ratioOfTotalTip = worker.hours * worker.rate / total_hours
+            worker.totalTip = total_tips * worker.ratioOfTotalTip
             worker.leftToPay = worker.totalTip
 
         # distribute each type of tender, starting with the larger
-        for tender in cashList:
-            while (tender.quantity > 0):
+        for tender in cash_list:
+            while tender.quantity > 0:
                 # prioritise whoever is owed more first
-                tempListOfWorkers = sortBiggerFirst(listOfWorkers)
-                for worker in tempListOfWorkers:
+                temp_list_of_workers = sortBiggerFirst(list_of_workers)
+                for worker in temp_list_of_workers:
                     if worker.leftToPay >= tender.value:
                         worker.breakdown[tender.indexForBreakdown] += 1
                         worker.leftToPay -= tender.value
@@ -306,60 +258,60 @@ def splitTips():
                             break
                     else:
                         break
-                if (tender.value > tempListOfWorkers[0].leftToPay):
+                if tender.value > temp_list_of_workers[0].leftToPay:
                     break
 
-        paidTips = 0
+        paid_tips = 0
 
         for worker in Worker.instances:
-            paidTips += worker.totalTip - worker.leftToPay
+            paid_tips += worker.totalTip - worker.leftToPay
 
-        st.write(f'\nsum of all owed tips is {totalTips}')
-        st.write(f'sum of all paid tips is {paidTips}')
+        st.write(f'\nsum of all owed tips is {total_tips}')
+        st.write(f'sum of all paid tips is {paid_tips}')
 
-        tipBoardData = []
+        tips_summary_data = []
         for instance in Worker.instances:
-            tipBoardData.append({
-                "name" : instance.name,
-                "50" : instance.breakdown[0],
-                "20" : instance.breakdown[1],
-                "10" : instance.breakdown[2],
-                "5" : instance.breakdown[3],
-                "2" : instance.breakdown[4],
-                "1" : instance.breakdown[5],
-                "0.5" : instance.breakdown[6],
-                "0.2" : instance.breakdown[7],
-                "owed" : round(instance.totalTip, 2),
-                "paid" : instance.tipsPaid,
-                "hours" : instance.hours,
-                "% of tips" : (instance.ratioOfTotalTip * 100),
+            tips_summary_data.append({
+                "name": instance.name,
+                "50": instance.breakdown[0],
+                "20": instance.breakdown[1],
+                "10": instance.breakdown[2],
+                "5": instance.breakdown[3],
+                "2": instance.breakdown[4],
+                "1": instance.breakdown[5],
+                "0.5": instance.breakdown[6],
+                "0.2": instance.breakdown[7],
+                "owed": round(instance.totalTip, 2),
+                "paid": instance.tipsPaid,
+                "hours": instance.hours,
+                "% of tips": (instance.ratioOfTotalTip * 100),
 
             })
 
-        TPD = pd.DataFrame(
-            tipBoardData
-        )
+        tips_summary = pd.DataFrame(tips_summary_data)
         st.write("Tips summary:")
-        st.dataframe(TPD, use_container_width=True)
+        st.dataframe(tips_summary, use_container_width=True)
 
-        cashLeftBoard = [{
-            "50" : cashList[0].quantity,
-            "20": cashList[1].quantity,
-            "10": cashList[2].quantity,
-            "5": cashList[3].quantity,
-            "2": cashList[4].quantity,
-            "1": cashList[5].quantity,
-            "0.50": cashList[6].quantity,
-            "0.20": cashList[7].quantity,
+        cash_summary_data = [{
+            "50": cash_list[0].quantity,
+            "20": cash_list[1].quantity,
+            "10": cash_list[2].quantity,
+            "5": cash_list[3].quantity,
+            "2": cash_list[4].quantity,
+            "1": cash_list[5].quantity,
+            "0.50": cash_list[6].quantity,
+            "0.20": cash_list[7].quantity,
 
         }]
 
-        CLB = pd.DataFrame(cashLeftBoard)
+        cash_summary = pd.DataFrame(cash_summary_data)
         st.write("what remains in the tip pool:")
-        st.dataframe(CLB, use_container_width=True)
+        st.dataframe(cash_summary, use_container_width=True)
         st.write("You might need to break bigger bills as I'm just an app and cannot always split it.")
     else:
         st.write("fill all the forms first you naughty sausage!")
+
+
 if st.button("DO IT"):
     splitTips()
 
